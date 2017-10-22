33b735ce2209208e4736364aaa334782ef6c24ac
mariogmazza <mariomazza01@gmail.com>
2017-10-21 23:33:19 -0400

    modal is working added some css / MVP

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -209,6 +209,7 @@ router.get("/articles/:id", function(req, res) {
       .populate("note")
       .then(function(dbArticle) {
         // If we were able to successfully find an Article with the given id, send it back to the client
+        console.log(dbArticle);
         res.json(dbArticle);
       })
       .catch(function(err) {
@@ -217,6 +218,29 @@ router.get("/articles/:id", function(req, res) {
       });
   });
 
+
+//   Character.remove({ name: 'Eddard Stark' }, function (err) {});
+
+  // Route for saving/updating an Article's associated Note
+router.get("/remove/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    Note
+      .remove({_id:req.params.id})
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.redirect("/saved");
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
 
 
